50f9a54a6b605154d77e572711d407600512ae8c
Sucho <anze.mendoza@gmail.com>
2021-10-13 18:09:51 -0300

    inicio de proyecto

---
+++ src/main/java/edu/sucho/libreriaweb/LibreriaWebApplication.java
@@ -0,0 +1,13 @@
+package edu.sucho.libreriaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreriaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibreriaWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/sucho/libreriaweb/LibreriaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.sucho.libreriaweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibreriaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
